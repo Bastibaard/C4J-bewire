ff3fa4ba4299e56af09b03ef271d831559c0da9d
Thomas Van Nevel <thomas.vannevel@hotmail.com>
2021-12-02 10:13:07 +0100

    Added sout in startup

---
+++ src/main/java/be/hackthefuture/c4j/StartUp.java
@@ -0,0 +1,13 @@
+package be.hackthefuture.c4j;
+
+public class StartUp {
+
+    public static void main(String[] args) {
+        new StartUp().run();
+    }
+
+    private void run() {
+        System.out.println("Hello C4J!");
+    }
+
+}
