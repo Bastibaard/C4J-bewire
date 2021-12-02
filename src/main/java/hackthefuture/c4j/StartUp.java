bd07c7118ef0b7785e8deeeca631677ea58728bd
Thomas Van Nevel <thomas.vannevel@hotmail.com>
2021-12-02 15:27:07 +0100

    added some things to startup

--- src/main/java/hackthefuture/c4j/StartUp.java
+++ src/main/java/hackthefuture/c4j/StartUp.java
@@ -22,8 +22,7 @@ public class StartUp {
         HttpResponse<Case> caseInvestigations = testApi.GetCaseInvestigations("07fd951b-6ec3-430e-a596-02bf2a819c52");
         System.out.println(caseInvestigations.getBody().getInvestigations().get(0).getId());
 
-        JSONObject obj = new JSONObject(caseInvestigations.getBody().getInvestigationById("761da2b7-1ca7-4a96-ad02-ed874d892e52").getInvestigationParameters());
-        System.out.println(Investigations.CheckDays(obj));
+
 
         /* System.out.println(Investigations.StringToHex("9Oqopcyl4RQa"));
         System.out.println(Investigations.Base64Decode("MVdNcmpYeFBqTlJyR2hJcklHSWttQTlhaXQzZHBmbHZEZUYzNmFrbGNxWg=="));
@@ -31,5 +30,24 @@ public class StartUp {
         System.out.println(Investigations.ReverseString("Dw7mfcXT2BzlRWSlCCsPuSKIAbHcKX7MFO0WmLcSFewn5KcwPMOt4OngHAySEOfHXuWAA9FVJ0c1uubZe8EZkuzwNU7QgaKKkL8l2hjzrgGwxU5nfbgrAgrwH63IG"));
         System.out.println(Investigations.isAlphabetical("001244799AAACCEFHIIIJKMOPPPTTUUVWWXXYYYZZaabdefgijklmmnpprrrsuuvvwwxxyy"));
         */
+
+        // Oplossen
+
+        String strToHex = caseInvestigations.getBody().getInvestigationById("83d83ee8-c71f-4d2b-aad8-35b231495b18").getInvestigationParameters();
+        String base64 = caseInvestigations.getBody().getInvestigationById("098fbde5-ffa2-4e60-a218-c7d6b325a531").getInvestigationParameters();
+        String fibonacci = caseInvestigations.getBody().getInvestigationById("c0613a4e-8f5d-4a17-9ff6-7fd961ef4f59").getInvestigationParameters();
+        String reverseThis = caseInvestigations.getBody().getInvestigationById("7c53a11c-bd65-4bfd-95e1-fa5eee4bff43").getInvestigationParameters();
+        JSONObject dateStuff = new JSONObject(caseInvestigations.getBody().getInvestigationById("761da2b7-1ca7-4a96-ad02-ed874d892e52").getInvestigationParameters());
+        String alphabet = caseInvestigations.getBody().getInvestigationById("5e5903fa-912d-498a-b22c-55755bdb8dc0").getInvestigationParameters();
+
+        Investigations.StringToHex(strToHex);
+        Investigations.Base64Decode(base64);
+        //Investigations.FindMissingNumbers(fibonacci); //this one will not work -> opdracht verkeerd begrepen
+        Investigations.ReverseString(reverseThis);
+        Investigations.CheckDays(dateStuff);
+        Investigations.isAlphabetical(alphabet);
+
+
+
     }
 }
