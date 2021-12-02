93744e6b149ae6e09995fa0a1762cf789dabe29e
Thomas Van Nevel <thomas.vannevel@hotmail.com>
2021-12-02 13:14:25 +0100

    bruteforce failed, thank you come again

--- src/main/java/hackthefuture/c4j/investigations/Investigations.java
+++ src/main/java/hackthefuture/c4j/investigations/Investigations.java
@@ -7,16 +7,16 @@ import java.nio.charset.StandardCharsets;
 public abstract class Investigations {
 
     public static String StringToHex(String input){
-        String id = "83d83ee8-c71f-4d2b-aad8-35b231495b18";
+        // id = 83d83ee8-c71f-4d2b-aad8-35b231495b18
         String hexedInput = Hex.encodeHexString(input.getBytes(StandardCharsets.UTF_8));
         // Zoekt naar 2 opeenvolgende karakters en vervangt deze door dezelfde karakters, maar gevolgd door een spatie
         String res = hexedInput.replaceAll("..", "$0 ").toUpperCase();
         // Echter laatste spatie moet nog verwijderd worden (not sure of het anders als fout zou aangeduid worden
-        // Geeft normaal string "39 4F 71 6F 70 63 79 6C 34 52 51 61" terug
         return res.substring(0,res.length()-1);
     }
 
     public static String Base64Decode(String input){
+        //id = 098fbde5-ffa2-4e60-a218-c7d6b325a531
         byte[] base64decoded = Base64.decodeBase64(input);
         return new String(base64decoded, StandardCharsets.UTF_8);
     }
